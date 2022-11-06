d52b240e852b3aa2f41002cc305e14f603cf8236
arukendev <iamjaehyun4@gmail.com>
2022-11-06 18:54:40 +0900

    spanish sunshine

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210, 200, 15, 100);
-ctx.fillRect(350, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
-ctx.arc(290, 150, 40, 0, 2 * Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(275, 140, 7, Math.PI, 2 * Math.PI);
-ctx.arc(305, 140, 7, Math.PI, 2 * Math.PI);
-ctx.fill();
+const colors = [
+  "#33d9b2",
+  "#34ace0",
+  "#706fd3",
+  "#ffda79",
+  "#ffb142",
+  "#ff793f",
+  "#ff5252",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
